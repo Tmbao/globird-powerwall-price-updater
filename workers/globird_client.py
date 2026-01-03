c1f9f73b634f8801219826165398b3401893fe2d
Bao Truong <tmbao.1995@gmail.com>
2026-01-04 07:32:33 +1100

    Update 2026 globird prices

--- workers/globird_client.py
+++ workers/globird_client.py
@@ -13,13 +13,12 @@ class GlobirdClient:
     Globird electricity prices are as follows:
      - Buy prices:
         - From 6PM to 8PM: $AU1.50 per kWh
-        - From 4PM to 11PM (outside of 6PM to 8PM): $AU0.46 per kWh
+        - From 4PM to 11PM (outside of 6PM to 8PM): $AU0.484 per kWh
         - From 11AM to 2PM: $AU0.0 per kWh
-        - Other times: $AU0.31 per kWh
+        - Other times: $AU0.341 per kWh
      - Sell prices:
         - From 6PM to 8PM: $AU0.15 per kWh
-        - From 4PM to 9PM (outside of 6PM to 8PM): $AU0.9 per kWh
-        - From 11AM to 2PM: $AU0.0 per kWh
+        - From 4PM to 9PM (outside of 6PM to 8PM): $AU0.8 per kWh
         - Other times: $AU0.05 per kWh
      - Resolution: 30 minutes
     """
@@ -31,23 +30,23 @@ class GlobirdClient:
         if datetime.time(18, 0) <= time < datetime.time(20, 0):
             return 1.50
         elif datetime.time(16, 0) <= time < datetime.time(23, 0):
-            return 0.46
+            return 0.484
         elif datetime.time(11, 0) <= time < datetime.time(14, 0):
             return 0.0
         else:
-            return 0.31
+            return 0.341
 
     def _get_sell_price(self, time: datetime.time) -> float:
         if datetime.time(18, 0) <= time < datetime.time(20, 0):
             return 0.15
         elif datetime.time(16, 0) <= time < datetime.time(21, 0):
-            return 0.09
+            return 0.08
         elif datetime.time(11, 0) <= time < datetime.time(14, 0):
             return 0.0
         elif datetime.time(10, 0) <= time < datetime.time(15, 0):
-            return 0.01
+            return 0.0
         else:
-            return 0.05
+            return 0.0
 
     def get_prices(self) -> List[SimplePrice]:
         """
